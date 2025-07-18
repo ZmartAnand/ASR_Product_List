721db46f23d16ae8aeadc074f952e7f00a4e8cec
Anand <s.anandsiva7897@gmail.com>
2025-07-18 15:52:50 +0530

    Changed

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip }  from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip, IonButton, IonButtons }  from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-product',
   templateUrl: 'product.page.html',
   styleUrls: ['product.page.scss'],
-  imports: [IonChip, IonLabel, IonItem, IonAccordion, IonAccordionGroup, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonButtons, IonButton, IonChip, IonLabel, IonItem, IonAccordion, IonAccordionGroup, IonSearchbar, IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class ProductPage {
-  constructor() {}
+  
+  constructor(private router: Router) {}
+  goToNext() {
+    this.router.navigate(['/next-page']);
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { triangle, ellipse, square, list, pricetag, time} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ triangle, ellipse, square,list,pricetag,time});
   }
 }
