6b6402fb27396f3c6c87d48a4076668a94280435
Anand <s.anandsiva7897@gmail.com>
2025-07-18 16:49:04 +0530

    changed

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, list, pricetag, time} from 'ionicons/icons';
+import { list, pricetag, time, create, bag} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square,list,pricetag,time});
+    addIcons({ list,pricetag,time,create,bag});
   }
 }
